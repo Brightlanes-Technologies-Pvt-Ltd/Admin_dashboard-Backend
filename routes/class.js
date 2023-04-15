c1aea952c27928b893cd074f4afa20b4956ee162
bharatgl <goyalbharat9977@gmail.com>
2023-04-15 16:49:21 +0530

    feat: course API v.1.0

--- routes/class.js
+++ routes/class.js
@@ -1,34 +1,38 @@
-const express = require("express");
+const express = require('express');
 const {
-  createClass,
-  createClassesFromCSV,
-  getAllClasses,
-  teacherUpdateClass,
-  adminUpdateClass,
-  deleteClass,
-  getClassById,
-} = require("../controllers/classController");
-const { customRole } = require("../middlewares/customRoles");
+	createClass,
+	createClassesFromCSV,
+	getAllClasses,
+	teacherUpdateClass,
+	adminUpdateClass,
+	deleteClass,
+	getClassById,
+	getAllClassesOfAcourse
+} = require('../controllers/classController');
+const { customRole } = require('../middlewares/customRoles');
 const classRouter = express.Router();
-const { isUserLoggedIn } = require("../middlewares/isUserLoggedIn");
+const { isUserLoggedIn } = require('../middlewares/isUserLoggedIn');
 
-classRouter.route("/create").post(isUserLoggedIn, customRole, createClass);
-
-classRouter
-  .route("/createfromcsv")
-  .post(isUserLoggedIn, customRole, createClassesFromCSV);
-
-classRouter.route("/").get(isUserLoggedIn, getAllClasses);
-
-classRouter.route("/update/:id").put(isUserLoggedIn, teacherUpdateClass);
+classRouter.route('/create').post(isUserLoggedIn, customRole, createClass);
 
 classRouter
-  .route("/adminupdate/:id")
-  .put(isUserLoggedIn, customRole, adminUpdateClass);
+	.route('/createfromcsv')
+	.post(isUserLoggedIn, customRole, createClassesFromCSV);
+
+classRouter.route('/').get(isUserLoggedIn, getAllClasses);
+
+classRouter.route('/update/:id').put(isUserLoggedIn, teacherUpdateClass);
 
 classRouter
-  .route("/delete/:id")
-  .delete(isUserLoggedIn, customRole, deleteClass);
+	.route('/adminupdate/:id')
+	.put(isUserLoggedIn, customRole, adminUpdateClass);
 
-classRouter.route("/getclassbyid/:id").get(isUserLoggedIn, getClassById);
+classRouter
+	.route('/delete/:id')
+	.delete(isUserLoggedIn, customRole, deleteClass);
+
+classRouter.route('/getclassbyid/:id').get(isUserLoggedIn, getClassById);
+
+classRouter.route('/:courseId').get(isUserLoggedIn, getAllClassesOfAcourse);
+
 module.exports = classRouter;
--- schema/courseSchema.js
+++ schema/courseSchema.js
@@ -1,21 +1,18 @@
 const mongoose = require('mongoose');
 
-const classSchema = new mongoose.Schema(
+const courseSchema = new mongoose.Schema(
 	{
-		className: {
+		courseName: {
 			type: String,
 			required: true
 		},
-		faculty: {
-			type: mongoose.Schema.Types.ObjectId,
-			required: true,
-			ref: 'User'
-		},
-		classOf: {
-			type: mongoose.Schema.ObjectId,
-			required: true,
-			ref: "Course"
-		},
+		faculities: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				required: true,
+				ref: 'Teacher'
+			}
+		],
 		startDate: {
 			type: Date,
 			required: true
@@ -24,15 +21,11 @@ const classSchema = new mongoose.Schema(
 			type: Date,
 			required: true
 		},
-		classHours: {
-			type: Number,
-			required: true
-		},
 		description: {
 			type: String,
 			required: true
 		},
-		category: {
+		subject: {
 			type: String,
 			required: true
 		},
@@ -41,11 +34,11 @@ const classSchema = new mongoose.Schema(
 			required: true,
 			default: 'Active'
 		},
-		agendas: [
-			{
-				type: String
-			}
-		],
+		enrolledStudents: {
+			type: Number,
+			require: true,
+			default: 0
+		},
 		created_by: {
 			type: mongoose.Schema.Types.ObjectId,
 			required: true,
@@ -64,4 +57,4 @@ const classSchema = new mongoose.Schema(
 		timestamps: true
 	}
 );
-module.exports = mongoose.model('Class', classSchema);
+module.exports = mongoose.model('Course', courseSchema);
